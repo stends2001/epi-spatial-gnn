e6ec0041ec4212fa8a7166e692d6c29bab465566
de-SchrijverS <de-schrijvers@hpc-login02.rki.local>
2026-08-24 15:28:10 +0200

    added Disease

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,7 +2,7 @@ from .pathmanager import PathManager
 from .io import write_yaml_file, list_files, save_mapping_dict, load_mapping_dict
 from .registries import registry_method, get_registered_methods
 from .sets import reorder_dict, compare_sets
-from .types import Country, AdminLevel, ColumnType, DataSetSplit
+from .types import Country, AdminLevel, ColumnType, DataSetSplit, Disease
 from .textformatting import checkmark, crossmark, align, return_header_line, section
 from .exceptions import (
     AttributeNotFound, MissingColumnError, InvalidDataSetError, UnequalSetsError, MethodNotInRegistry,
--- src/utils/types.py
+++ src/utils/types.py
@@ -2,6 +2,6 @@ from typing import Literal
 
 Country         = Literal['germany','hungary']
 AdminLevel      = Literal['nuts1', 'nuts2', 'nuts3']
-
+Disease         = Literal['influenza','chickenpox']
 ColumnType      = Literal['context','feature','target','pred','split']
 DataSetSplit    = Literal['train','val','test']
