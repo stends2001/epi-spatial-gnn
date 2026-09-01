cf99a398a086f7413b8f2851647320016b5089a6
de-SchrijverS <de-schrijvers@hpc-login02.rki.local>
2026-09-01 16:18:00 +0200

    modularized lossmanager

---
+++ src/models/gnnmodels/utils/exceptions.py
@@ -0,0 +1,15 @@
+
+class InvalidOptimizerError(Exception):
+    def __init__(self, optimizer_name: str, supported_optimizers: list[str]):
+        message = f'Invalid optimizer {optimizer_name}. Supported optimzers are {supported_optimizers}'
+        super().__init__(message)        
+
+class InvalidLossError(Exception):
+    def __init__(self, loss_name: str, supported_losses: list[str]):
+        message = f'Invalid loss {loss_name}. Supported losses are {supported_losses}'
+        super().__init__(message)         
+
+class InvalidSchedulerError(Exception):
+    def __init__(self, scheduler_name: str, supported_schedulers: list[str]):
+        message = f'Invalid scheduler {scheduler_name}. Supported schedulers are {supported_schedulers}'
+        super().__init__(message)      
---
+++ src/models/gnnmodels/utils/lossmanager/__init__.py
@@ -0,0 +1,1 @@
+from .lossmanager import LossManager
