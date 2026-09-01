41ab8537ec60825b02a2cee19dce34bce5eaf916
de-SchrijverS <de-schrijvers@hpc-login02.rki.local>
2026-09-01 10:56:17 +0200

    added representations

--- src/models/basemodel/appearance_mixin.py
+++ src/models/basemodel/appearance_mixin.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 import re
 
-from ..utils import model_colors
+from ..utils import model_colors, ModelStatus
+from ...utils import section, checkmark, crossmark
 
 if TYPE_CHECKING:
     from ...dataloading.columnregistration import ColumnRegistry
@@ -11,12 +12,16 @@ if TYPE_CHECKING:
 
 class ModelAppearanceMixin:
     """ 
-    Mixin class to ``BaseModel`` that deals with the model's appearance.
+    Mixin class to ``BaseModel`` that deals with the model's appearance 
+    (colors, name, representation)
     """
     name : str
     model_class : str
     epiconfig : EpiConfig
     column_registration : ColumnRegistry
+    status_dict : dict[ModelStatus, bool]
+    model_class : str 
+    verbose : int    
 
     def _get_model_color(self) -> str:
         """returns model-color in string format based on the lookup in model_colors"""
@@ -36,3 +41,58 @@ class ModelAppearanceMixin:
         name = re.sub(r"_+", "_", name)
     
         return name.strip("_")
+
+    def _print_status_update(self, status: ModelStatus):
+        """Print status update depending on `verbose`"""
+
+        if self.verbose <= 0:
+            return
+
+        if status == "model_initialized" and self.verbose > 1:
+            self._print_header()
+        else:
+            print(f"{status} {checkmark}")
+
+    def _print_header(self):
+        """Print formatted model header."""
+
+        total_width     = 50
+        title           = self.name
+
+        inner_width     = total_width - 4
+        centered_title  = title.center(inner_width)
+
+        print(
+            "\n"
+            + "=" * total_width + "\n"
+            + f"=={centered_title}==\n"
+            + "=" * total_width
+        )
+
+    def __repr__(self) -> str:
+        """minimal representation"""
+        return f"{self.__class__.__name__}(name={self.name!r})"    
+
+    def __str__(self) -> str:
+        """extensive representation"""
+        all_keys = (
+            ['name', 'model_class'] + list(self.status_dict.keys())
+        )
+
+        width = max(len(k) for k in all_keys) if all_keys else 20
+        
+        # Build output
+        lines = [f'<{self.__class__.__name__}(']
+        lines.append('')
+        general_items = {'name': self.name, 'model_class': self.model_class}
+        lines.extend(section('generics', general_items, width))
+        lines.append('')
+        
+        # Status section
+        status_items = {k: f"{checkmark}" if v else f"{crossmark}" for k, v in self.status_dict.items()}
+        lines.extend(section('status', status_items, width))
+        lines.append('')
+        
+        lines.append(')>')
+        
+        return '\n'.join(lines)
