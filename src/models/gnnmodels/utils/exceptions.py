1b3336b1f0aa0809ecebbd55ff42985afb1fd607
de-SchrijverS <de-schrijvers@hpc-login02.rki.local>
2026-09-02 15:51:04 +0200

    gnn-predictions exceptions

--- src/models/gnnmodels/utils/exceptions.py
+++ src/models/gnnmodels/utils/exceptions.py
@@ -1,4 +1,3 @@
-
 class InvalidOptimizerError(Exception):
     def __init__(self, optimizer_name: str, supported_optimizers: list[str]):
         message = f'Invalid optimizer {optimizer_name}. Supported optimzers are {supported_optimizers}'
@@ -13,3 +12,15 @@ class InvalidSchedulerError(Exception):
     def __init__(self, scheduler_name: str, supported_schedulers: list[str]):
         message = f'Invalid scheduler {scheduler_name}. Supported schedulers are {supported_schedulers}'
         super().__init__(message)      
+
+class UnexpectedDataShape(Exception):
+    """
+    errors are to be raised!
+    """    
+    def __init__(self, received_obj: str, expected_obj: str, context: str):
+        message = f"context: {context} \nExcpected {expected_obj}, got {received_obj}"
+        super().__init__(message)
+
+class InconsistentDataShape(Exception):
+    def __init__(self, message):
+        super().__init__(message)    
