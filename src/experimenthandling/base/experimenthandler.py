63e654986eded9d144a7338df3dffd93363b01c8
de-SchrijverS <de-schrijvers@hpc-login02.rki.local>
2026-09-08 10:56:08 +0200

    added documentation

--- src/experimenthandling/base/experimenthandler.py
+++ src/experimenthandling/base/experimenthandler.py
@@ -11,7 +11,51 @@ from ...dataloading import EpiConfig, GraphDataBuilder, BaseLineDataBuilder
 from ...models.gnnmodels.gnnmodel import GNNModel
 
 class ExperimentHandler:
+    """ 
+    Parent class of xperiment-handling classes, namely:
+    - ``ExperimentRunner``
+    - ``ExperimentLoader``
+    - ``ExperimentAnalyzer``(second degree; this is a sub-class to ExperimentLoader)
+    
+    This parent class only deals with the paths based on the experiment_name.
+    This class should not be called in itself, but rather, its subclasses should.
+    Also defines a `_get_dlm()` method which is shared among the subclasses.
 
+    Parameters
+    ----------
+    experiment_name: str 
+        Name of the experiment. This string should be identical to the directory in 
+        which the models and configs are saved.     
+    
+    Methods
+    -------
+    `_get_databuilder()`
+        Returns a specific DataBuilder (BaseLineDataBuilder | GraphDataBuilder)
+    
+    Attributes
+    ----------
+    ``dataloadermanagers`` : dict[int | str | float, ExperimentDataBuilders] | None
+        Dictionary of ExperimentDataBuilders per value of the experiment's variable.
+        ``_get_databuilder`` returns a specific dataloadermanager from this attribute.
+    ``epicfg``: EpiConfig
+        Large configuration class that dictates which data to load. NOTE that
+        ``ExperimentRunner`` and ``ExperimentLoader`` deal different with this class.
+    ``expcfg`` : ExperimentConfig
+        Configuration class that describes the experiment.
+
+    See Also
+    --------
+    ``ExperimentConfig``
+        Configuration class that describes the experiment.    
+    ``EpiConfig``
+        Large configuration class that dictates which data to load.
+    ``ExperimentDataBuilders``
+        Simple container that stores DataBuilders for each combination of parameters.
+
+    Downstream
+    ----------
+    ...
+    """    
     databuilders: dict[int | str | float, ExperimentDataBuilders] | None = None
     epicfg : EpiConfig           
     expcfg : ExperimentConfig
@@ -22,13 +66,13 @@ class ExperimentHandler:
     def __init__(self, 
                  experiment_name : str):
         
-        self.experiment          = experiment_name
+        self.experiment = experiment_name
         
         # paths
-        self.pm                  = PathManager()
-        self.path_exp_root       = self.pm.outcomes
-        self.path_exp            = self.pm.outcomes / experiment_name
-        self.exp_exists          = self._exp_directory_exists()   
+        self.pm = PathManager()
+        self.path_exp_root = self.pm.outcomes
+        self.path_exp = self.pm.outcomes / experiment_name
+        self.exp_exists = self._exp_directory_exists()   
 
     def _exp_directory_exists(self) -> bool:
         """
@@ -59,7 +103,7 @@ class ExperimentHandler:
                  
         return True          
 
-    def _get_dlm(self, 
+    def _get_databuilder(self, 
                  modelclass : str, 
                  varvalue : int | float | str, 
                  graph : str | None = None) -> GraphDataBuilder | BaseLineDataBuilder:
