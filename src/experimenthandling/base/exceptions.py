f57b7bb78e20775864ab02f2249e757b63ba6f0e
de-SchrijverS <de-schrijvers@hpc-login02.rki.local>
2026-09-08 09:46:32 +0200

    initializing exceptions

---
+++ src/experimenthandling/base/exceptions.py
@@ -0,0 +1,11 @@
+class IncompatibleExperimentConfigs(Exception):
+    def __init__(self, msg: str):
+        super().__init__(msg)
+
+class DataLoaderManagerError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)       
+
+class ExperimentDirectoryNotFoundError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)      
